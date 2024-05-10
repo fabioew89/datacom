bce68704ed6d5261e6645674ef133fd8ace88982
fabioew89 <fabioew89@gmail.com>
2024-05-10 19:01:47 -0400

    modified:   remote-upgrade.sh

--- remote-upgrade.sh
+++ remote-upgrade.sh
@@ -10,34 +10,47 @@ YELLOW="\e[33;1m"
 RESET="\e[0m"
 
 ssh_output(){
-    ssh_output="$(sshpass -f password ssh -o StrictHostKeyChecking=no "$USERNAME"@"$ip_host" "$COMMAND")"
+    ssh_output="$(sshpass -f password ssh -o StrictHostKeyChecking=no \
+    "$USERNAME"@"$ip_host" "$COMMAND")" > /dev/null 2>&1
     get_device_hostname="$(echo "$ssh_output" | grep -i hostname | cut -d ' ' -f 2)"
     get_device_firmware="$(echo "$ssh_output" | tail -2)"
-    get_device_firmware_version="$(echo "$ssh_output" | grep Active | cut -d "." -f 1)"
+    get_device_firmware_version="$(echo "$ssh_output" | grep Active | cut -d "-" -f 1)"
 }
 
 firmware_check_version(){
-    [ $get_device_firmware_version -ge 8 ] && \
+    [ "$get_device_firmware_version" -ge 8 ] && \
     echo -e "${GREEN}\nUp to date! 😁${RESET}" || \
     echo -e "${YELLOW}\nNeeds updating! 😭${RESET}"
+}
+
+firmware_check_version_check() {
+    # Extrai a parte numérica da versão do firmware
+    firmware_version=$(echo "$get_device_firmware_version" | awk -F. '{print $1"."$2}')
+
+    if (( $(echo "$firmware_version >= 9.4" | bc -l) )); then
+        echo -e "${GREEN}Up to date! 😁${RESET}"
+    elif (( $(echo "$firmware_version < 9.4" | bc -l) )); then
+        echo -e "${YELLOW}Needs updating! 😭${RESET}"
+    else
+        echo 'versão do firmware desconhecida'
+    fi
 }
 
 
 for ip_host in 100.127.0.{1..100}; do
     if ping -c 3 -q -W 3 "$ip_host" > /dev/null; then
+        
         ssh_output
         
-        echo -e "${GREEN}\n[INFO] - Geting information about "$get_device_hostname" - "$ip_host"${RESET}\n\
-        \n$get_device_hostname\n$get_device_firmware"
+        echo -e "${GREEN}\n[INFO] - Geting information about $get_device_hostname - $ip_host${RESET}\n\
+        \n$get_device_hostname\n$get_device_firmware\n"
         
-        firmware_check_version
+        firmware_check_version_check
 
     else
-        echo -e "${RED}\n[INFO] - Sorry, better luck next time "$ip_host"${RESET}"
+        echo -e "${RED}\n[INFO] - Sorry, better luck next time $ip_host${RESET}"
     fi
 
     # JUST SEPARATOR
-    echo
-    for i in $( seq 15 ); do echo -n "##### " done
-    echo   
+    echo ; for _ in $( seq 15 ); do echo -n "##### "; done ; echo
 done
