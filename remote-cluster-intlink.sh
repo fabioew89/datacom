ac34ecfb5bc4a5935b76f725ee80df09c97b4d5d
fabioew89 <fabioew89@gmail.com>
2024-05-20 15:21:37 -0400

    deleted:    remote-cluster-intlink.sh

--- remote-cluster-intlink.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for host in {73..74}; do
-    sshpass -f password ssh -o StrictHostKeyChecking=no "fabio.ewerton"@100.127.0.$host  < "config/config-dmos-cluster-intlink.md"
-    echo " "
-    echo "##################################################"
-    echo " "
-done
